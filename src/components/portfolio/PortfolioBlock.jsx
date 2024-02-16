7488262bffc636ba5a2d1f08fbb7928fc2ef9041
Enrique Sabariego <quiquesaba@gmail.com>
2024-02-16 11:17:22 +0100

    details

--- src/components/portfolio/PortfolioBlock.jsx
+++ src/components/portfolio/PortfolioBlock.jsx
@@ -4,14 +4,14 @@ import {Box} from "@mui/material";
 import Style from '../BaseLayout.module.scss'
 
 function PortfolioBlock(props) {
-   const {image, live, source, title, className} = props;
+   const {image, live, source, title, darkMode} = props;
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
          <Box component={'img'} src={image} alt={'mockup'}/>
          <h1 style={{fontSize: '2rem'}}>{title}</h1>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'1rem'}
               alignItems={'center'} fontSize={'1.3rem'} py={'2rem'}>
-               {!Style.light ? 
+               {!darkMode ? 
                   <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
                      🖥️<IconLink link={live} title={'Demo'} />
                   </Box>
@@ -21,11 +21,11 @@ function PortfolioBlock(props) {
                   </Box>
                }
                
-               {!Style.light ? 
+               {!darkMode ? 
                   <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
                      {"</>"}<IconLink link={source} title={'Source Code'}/>
                   </Box>
-               : 
+               :
                   <Box p={1} border={'2px solid white'} borderRadius={'25px'}>
                      {"</>"}<IconLink link={source} title={'Source Code'}/>
                   </Box>
