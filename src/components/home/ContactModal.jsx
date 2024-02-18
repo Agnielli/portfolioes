75a1f563c36305d92ba3c49bafede03421c0a55b
Enrique Sabariego <quiquesaba@gmail.com>
2024-02-18 19:48:45 +0100

    modal component

---
+++ src/components/home/ContactModal.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
+export const ContactModal = ({form, sendEmail, show, handleClose}) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Contact me</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form ref={form} onSubmit={sendEmail}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                  <Form.Label>Your email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="name@example.com"
+                    name="email"
+                    autoFocus
+                    as="textarea" rows={1}
+                    style={{ resize: 'none', overflow: 'auto' }}
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                  
+                >
+                  <Form.Label>Send me a message</Form.Label>
+                  <Form.Control as="textarea" rows={3} name="message"/>
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className="btn-dark" onClick={handleClose}>
+                Close
+              </Button>
+              <Button className="btn-dark" onClick={sendEmail}>
+                Send message
+              </Button>
+        </Modal.Footer>
+      </Modal>
+  )
+}
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,11 +7,9 @@ import SocialIcon from "./SocialIcon";
 import {Box} from "@mui/material";
 import {info} from "../../info/Info";
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
 import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import emailjs, { send } from '@emailjs/browser';
+import { ContactModal } from './ContactModal';
 
 
 emailjs.init('0eFM8pIIFIQo_Kjmt');
@@ -90,42 +88,15 @@ export default function Home() {
                ))}
             </Box>
          </Box>
-         <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Contact me</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form ref={form} onSubmit={sendEmail}>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Your email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                name="email"
-                autoFocus
-                as="textarea" rows={1}
-                style={{ resize: 'none', overflow: 'auto' }}
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-              
-            >
-              <Form.Label>Send me a message</Form.Label>
-              <Form.Control as="textarea" rows={3} name="message"/>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button className="btn-dark" onClick={handleClose}>
-            Close
-          </Button>
-          <Button className="btn-dark" onClick={sendEmail}>
-            Send message
-          </Button>
-        </Modal.Footer>
-      </Modal>
+
+            <ContactModal 
+               emailjs = {emailjs}
+               form = {form}
+               sendEmail={sendEmail}
+               show={show}
+               handleClose={handleClose}
+            />
+
       </Box>
    )
 }
